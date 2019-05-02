bbc8b71f525574e762a08ef3f7250d07aed92666
涛 <34599053+yitao-yu@users.noreply.github.com>
2019-05-02 07:58:02 -0400

    Update streetMap.java

--- streetMap.java
+++ streetMap.java
@@ -1,46 +1,65 @@
+import java.awt.Graphics;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 
-public class streetMap extends JComponent {
-	
-	int width = getWidth();
-	int height = getHeight();
-	
-	HaversineAlgorithm harvesine = new HaversineAlgorithm();
-
-	public Graph buildGraph(String x){
-		try {
-			BufferedReader in = new BufferedReader(new FileReader("input"));
-			//String x;
-			try {
-				x = in.readLine();
-				String[] ar = x.split("	");
-				
-				int i = 0;
-				while(x!=null) {
-					
-				}
-				
-				
-				
-			} catch (IOException e) {
-				System.out.println("file is empty");
+public class StreetMap extends JComponent {
+	public static void main(String[] args) {
+		File f = new File(args[0]);
+		Graph g = new Graph();
+		g.buildGraph(f);
+		
+		boolean ShouldShow = false;
+		boolean ShouldFind = false;
+		int startindex = 0;
+		
+		for(int i = 1; i< args.length-1; i++) {
+			if(args[i] == "-- show") {
+				ShouldShow = true;
+			}else if(args[i] == "-- directions") {
+				ShouldFind = true;
+				startindex = i+1;
 			}
-		} catch (FileNotFoundException e) {
-			System.out.println("can't find file");
 		}
 		
+		if(ShouldFind) {
+			Node start = new Node();
+			Node end = new Node();
+			for(Node n: g.nodes) {
+				if(n.ID.equals(args[startindex])) {
+					start = n;
+				}
+				if(n.ID.equals(args[startindex +1])) {
+					end = n;
+				}
+			}
+			try {
+				ArrayList<Graph.Edge> path = g.PathFinder(start, end);
+				
+				for(Graph.Edge e: path) {
+					System.out.println(e.id);
+				}
+			}catch(NullPointerException n) {
+				System.out.println("didn't input the start ID and end ID correctly");
+			}
+		}
 		
+		if(ShouldShow) {
+			JFrame frame = new JFrame();
+			Graphics G = frame.getGraphics();
+			frame.add(g);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setSize(1700,1700);
+			frame.setVisible(true);
+			g.paintComponent(G);
+		}
 		
-		
-		
-		
-		
-		return null;
 	}
 }
 	
