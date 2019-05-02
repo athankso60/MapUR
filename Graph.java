2a1efb873ecb579567523028214e2716a2271766
athankso60 <44515526+athankso60@users.noreply.github.com>
2019-05-01 20:56:57 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,15 +1,24 @@
 // A Java program for Bellman-Ford's single source shortest path 
 // algorithm. 
-import java.util.*; 
-import java.lang.*; 
+import java.util.*;
+
+import javax.swing.JComponent;
+
+import java.lang.*;
+import java.awt.Graphics;
 import java.io.*; 
 
 // A class to represent a connected, directed and weighted graph 
 class Graph 
 { 
+//	int height = getHeight();
+//	int width = getWidth();
 	
-	
-    private int V; // Number of vertices
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int V; // Number of vertices
 	// A class to represent a weighted edge in graph 
 	class Edge { 
 		Node src, dest;
@@ -39,57 +48,67 @@ class Graph
 		
 	} 
 
-	//int V, E; 
-	//ArrayList <Edge> edge = new ArrayList<Edge>(); 
+
 	ArrayList<Node> nodes = new ArrayList<Node>();
-//	private int dist[]; 
-//    private Set<Integer> settled; 
-//    private PriorityQueue<Node> pq; 
 
 	// Creates a graph with V vertices and E edges 
 	Graph() 
 	{ 
-		
+		for(int i = 0; i < nodes.size();i++) {
+			nodes.get(i).nodeNumber = i;
+		}
 	}
 	
 	
 	//Implementation of Dijsktra algorithm using priority Queue
-	class DPQ { 
+	public class DPQ { 
 	    private int dist[]; 
 	    private Set<Node> settled; 
 	    private PriorityQueue<Node> pq; 
 	    private int V; // Number of vertices 
-	    List<List<Node>> adj; 
+	    ArrayList<ArrayList<Edge>> adj; 
+	    public Graph graph;
 	  
-	    public DPQ(int V) 
+	    public DPQ(Graph g) 
 	    { 
 	    	//V would be the size of the nodes array
-	        this.V = V; 
+	        this.V = g.nodes.size(); 
 	        dist = new int[V]; 
 	        settled = new HashSet<Node>(); 
-	        pq = new PriorityQueue<Node>(V); 
+	        pq = new PriorityQueue<Node>(); 
+	        ArrayList<ArrayList<Edge>> a = new ArrayList<ArrayList<Edge>>();
+	        for(Node E:g.nodes) {
+	        	a.add(E.edges);
+	        	for(Edge e: E.edges) {
+	        		//System.out.println("edge id: " +e.id + "weight" + e.weight );
+	        	}
+	        }
+	        this.adj = a;
+	        this.graph = g;
 	    } 
 	  
 	    // Function for Dijkstra's Algorithm 
-	    public void dijkstra(List<List<Node> > adj, Node src) 
+	    public void dijkstra(Node src) 
 	    { 
-	        this.adj = adj; 
 	  
-	        for (int i = 0; i < V; i++) 
+	        for (int i = 0; i < V; i++) { 
 	            dist[i] = Integer.MAX_VALUE; 
-	  
+	            graph.nodes.get(i).weight = Integer.MAX_VALUE;
+	        }
 	        // Add source node to the priority queue 
 	        
 	        pq.add(new Node(src.ID,src.longitude,src.lattitude)); 
-	  
+	        
 	        // Distance to the source is 0 
 	        dist[src.nodeNumber] = 0; 
-	        src.weight = 0;
-	        while (settled.size() != V) { 
+	        //src.weight = 0;
+	        System.out.println("src Node Number = "+ src.nodeNumber);
+	        while (settled.size() != V && pq!=null) { 
 	  
 	            // remove the minimum distance node  
 	            // from the priority queue  
 	        	Node u = pq.remove(); 
+	        	System.out.println("u's id: " +u.ID);
 	  
 	            // adding the node whose distance is 
 	            // finalized 
@@ -97,7 +116,11 @@ class Graph
 	  
 	            e_Neighbours(u.nodeNumber); 
 	        } 
+	        
+	        
 	    } 
+	    
+	    
 	  
 	    // Function to process all the neighbours  
 	    // of the passed node 
@@ -109,17 +132,19 @@ class Graph
 	  
 	        // All the neighbors of v 
 	        for (int i = 0; i < adj.get(u).size(); i++) { 
-	            Node v = adj.get(u).get(i); 
+	            Node v = adj.get(u).get(i).src; 
 	  
 	            // If current node hasn't already been processed 
 	            if (!settled.contains(v)) { 
 	                edgeDistance = v.weight; 
 	                newDistance = dist[u] + edgeDistance; 
-	  
+	                
+	                System.out.println("e dis: "+edgeDistance);
+	                System.out.println("n dis: "+newDistance);
 	                // If new distance is cheaper in cost 
 	                if (newDistance < dist[v.nodeNumber]) 
-	                    dist[v.nodeNumber] = newDistance; 
-	  
+	                    dist[v.nodeNumber] = newDistance;
+	                	v.weight = newDistance;
 	                // Add the current node to the queue 
 	              //String iD, double longitude, double lattitude
 	                pq.add(new Node(v.ID, v.longitude,v.lattitude)); 
@@ -145,7 +170,9 @@ class Graph
 				int i = 0;
 				while((line=in.readLine())!=null) {
 					
-					String[] ar = line.split(" ");
+					String[] ar = line.split("	");
+					
+					System.out.println(ar[0]);
 					 
 					if(ar[0].equals("i")) {
 						Node Intersection = new Node(ar[1],Double.parseDouble(ar[2]),Double.parseDouble(ar[3]));
@@ -199,14 +226,49 @@ class Graph
 		
 	}
 	
-//	public static void main(String[] args) {
-//		File f = new File(".txt");
-//		Graph g = new Graph();
-//		g.buildGraph(f);
-//		//use g afterwards as the graph obj
-//		
-//		
-//	}
+	public void drawMap(Graph graph, Graphics g) {
+		ArrayList<Graph.Edge> edgeList = new ArrayList<Graph.Edge>();//adjacency list of current node
+		Node currNode;//current node
+		
+		for (int i = 0; i < graph.nodes.size(); i++ ) {
+			edgeList = graph.nodes.get(i).edges;
+			currNode = graph.nodes.get(i);
+			
+			for(int j=0; j<edgeList.size(); j++) {
+				g.drawLine((int)edgeList.get(i).src.longitude,(int) edgeList.get(i).src.lattitude,(int) edgeList.get(i).dest.longitude,(int)edgeList.get(i).dest.lattitude);//MIke will do the calculations to draw correctly
+			}
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		File f = new File("ur.txt");
+		Graph g = new Graph();
+		g.buildGraph(f);
+		//System.out.println(g.nodes.size());
+		
+		DPQ d = g.new DPQ(g);
+		d.dijkstra(g.nodes.get(1));
+		for(int i=0; i<d.dist.length; i++) {
+			System.out.print("d:  ");
+			System.out.println(d.dist[i]);
+			System.out.print("g:  ");
+			System.out.println(g.nodes.get(i).weight);
+
+		}
+
+			//System.out.println(g.nodes.get(1).nodeNumber);
+		
+		
+	
+	}
 
 	
 } 
